231c65d148a88a9354a7fee62e9ed104e33c62fc
Nikhil <yognik456@gmail.com>
2025-09-13 11:12:00 +0530

    commit

--- src/app.js
+++ src/app.js
@@ -5,8 +5,10 @@ import morgan from "morgan";
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import authRoutes from '#routes/auth.route.js';
+import securityMiddleware from './middleware/security.middleware.js';
 
 const app = express();
+
 app.use(helmet());
 app.use(cors())
 app.use(express.json());
@@ -15,7 +17,7 @@ app.use(cookieParser());
 
 app.use(morgan('combined',{stream:{write:(message)=>logger.info(message.trim())}}));
 
-
+app.use(securityMiddleware);
 
 app.get('/', (req, res) => {
     logger.info("Hello from Acquisition!")
